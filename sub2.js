6f0e91a48a761a5217027301f809d276a72896bc
yunju727 <80828786+yunju727@users.noreply.github.com>
2022-12-09 15:57:33 +0900

    edit js

--- sub2.js
+++ sub2.js
@@ -1,10 +1,10 @@
 function createA() {
-            let obj = document.getElementById("tags");
-            let newA = document.createElement("a");
-            let info_tag = prompt("추가로 필요한 태그를 입력하세요.", "");
-            let info_link = prompt("링크 입력하세요.")
-            newA.innerHTML = info_tag;
-            newA.setAttribute("href", info_link);
-            newA.setAttribute("class","tag")
-            obj.appendChild(newA);
-        }
+    let obj = document.getElementById("tags");
+    let newA = document.createElement("a");
+    let info_tag = prompt("추가로 필요한 태그를 입력하세요.", "");
+    let info_link = prompt("링크 입력하세요.")
+    newA.innerHTML = info_tag;
+    newA.setAttribute("href", info_link);
+    newA.setAttribute("class","tag")
+    obj.appendChild(newA);
+}
